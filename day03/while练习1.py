d232ff6e1c4c61113f59bb05e0bba8a1b33fd7a1
JiangHuYiXiao <1163270704@qq.com>
2018-09-03 22:34:29 +0800

    while循环

---
+++ day03/while练习1.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*-
+# # 练习1：打印1到100的偶数
+# # 方法1
+# count = 0
+# while count <=100:
+#     print ('loop',count)
+#     count += 2
+# print('-------end loop ------')
+
+# 方法2
+count = 0
+while count <= 100:
+    if count%2 ==0:
+        print('loop',count)
+        count += 1
+
+
+
+
+# 循环打印1-100，第50次不打印值，第60-80次打印对应值的平方。
+
+
