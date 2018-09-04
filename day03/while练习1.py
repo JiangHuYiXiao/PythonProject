9ade57c9fffc03126179e0aab5f6fc791e00bec6
JiangHuYiXiao <1163270704@qq.com>
2018-09-04 08:35:45 +0800

    while练习1

--- day03/while练习1.py
+++ day03/while练习1.py
@@ -1,11 +1,11 @@
 #-*- coding:utf-8 -*-
-# # 练习1：打印1到100的偶数
-# # 方法1
-# count = 0
-# while count <=100:
-#     print ('loop',count)
-#     count += 2
-# print('-------end loop ------')
+# 练习1：打印1到100的偶数
+# 方法1
+count = 0
+while count <=100:
+    print ('loop',count)
+    count += 2
+print('-------end loop ------')
 
 # 方法2
 count = 0
@@ -18,5 +18,16 @@ while count <= 100:
 
 
 # 循环打印1-100，第50次不打印值，第60-80次打印对应值的平方。
+
+a = 0
+while a <=100:
+
+    if a == 50:
+        pass #就是过，不打印任何东西
+    elif a >= 60  and a <=80:
+        print(a*a)
+    else:
+        print('loop:', a)
+    a += 1
 
 
